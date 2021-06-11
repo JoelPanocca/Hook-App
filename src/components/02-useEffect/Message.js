843f4b726fe5728c4d708a3e40218223dea7d9ca
Joel Panocca <panocca@hotmail.com>
2021-06-11 18:18:14 -0500

    Captura el movimiento del mouse y se renderiza sus coordenadas

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,14 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
 
 export const Message = () => {
-    useEffect(() => {        
-        return () => {
-            console.log("SE desmontó el mensaje");
-        }
-    }, [])
-    return (
-        <div>
-            <h3>Eres genial!!!</h3>
-        </div>
-    )
-}
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const { x, y } = coords;
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      const coordenadas = { x: e.x, y: e.y };
+      setCoords(coordenadas);
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h3>Eres genial!!!</h3>
+      <p> x: {x} , y: {y} </p>
+    </div>
+  );
+};
